597af53f3e06b0ad65079026803c550c0c599b51
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-05-13 16:26:23 -0500

    store added on index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import App from './App';
-import './index.css';
+import store from './Redux/configureStore';
 
 const container = document.getElementById('root');
-const root = createRoot(container);
+const root = ReactDOM.createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
