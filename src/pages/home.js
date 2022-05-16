5b54cfeee47d0b2641054c4599c05fee93ba1967
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-05-16 12:32:19 -0500

    home filtered only by South American Countries

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,8 +3,23 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getData } from '../Redux/Reducers/data';
 
 const Home = () => {
+  const sac = [
+    'Argentina',
+    'Bolivia',
+    'Brazil',
+    'Chile',
+    'Colombia',
+    'Ecuador',
+    'Guyana',
+    'Paraguay',
+    'Peru',
+    'Suriname',
+    'Uruguay',
+    'Venezuela',
+  ];
+
   const dispatch = useDispatch();
-  const data = useSelector((state) => state.data);
+  const data = useSelector((state) => state.data.filter((country) => sac.includes(country.name)));
 
   useEffect(() => {
     dispatch(getData());
@@ -27,7 +42,10 @@ const Home = () => {
           {data.map((country) => (
             <tr key={country.id} path={`${country.name}`}>
               <td>
-                <button type="button" onClick={() => handleRedirect(country.name)}>
+                <button
+                  type="button"
+                  onClick={() => handleRedirect(country.name)}
+                >
                   {country.name}
                 </button>
               </td>
